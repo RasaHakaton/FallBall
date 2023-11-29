5872adde7def201b4b1dddd2a84db375afd3abe3
RasaHakaton <rasa.hakaton@yahoo.com>
2023-11-29 02:41:55 +0100

    Added Randomization

--- Game/CollectABall/Ball.gd
+++ Game/CollectABall/Ball.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var godo = $Godo
+onready var box = $Box
 onready var speed = 250
 onready var rotspeed = 2.5
 
@@ -17,6 +19,7 @@ func _on_Player_body_entered(body):
 	position.y = -100
 	#randomize()
 	position.x = rand_range(704, 1216)
+	spritechoose()
 	speed = speed + 4.5
 	if rotspeed <= 12:
 		rotspeed = rotspeed + 0.0005
@@ -27,8 +30,18 @@ func _on_DeathZone_body_entered(body):
 	position.y = -100
 	#randomize()
 	position.x = rand_range(704, 1216)
+	spritechoose()
 	speed = speed + 5
 	if rotspeed <= 12:
 		rotspeed = rotspeed + 0.0008
 	elif rotspeed > 12:
 		rotspeed = 12
+
+func spritechoose():
+	#print (randi() % 2)
+	if randi() % 2 == 0:
+		godo.visible = true
+		box.visible = false
+	elif randi() % 2 == 1:
+		box.visible = true
+		godo.visible = false
