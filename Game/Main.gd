eca241e089e3ab545c6bf4c1feb99d0e64127b48
RasaHakaton <rasa.hakaton@yahoo.com>
2023-11-29 05:04:51 +0100

    Fully Implemented Core Gameplay Loop

--- Game/Main.gd
+++ Game/Main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var a = 0
 onready var score = 200
 onready var init = true
 onready var scorelabel = $GameplayLoop/ScoreHolder/HBoxContainer/Score
@@ -15,10 +16,13 @@ func _on_DeathZone_body_entered(body):
 
 func _physics_process(delta):
 	scorelabel.text = str(score)
-	if score <= 0 and init == false:
-		print ("gotov")
-	else: print ("negotov")
-
+	if score <= 0 and init == false and a == 1:
+		$GameplayLoop.visible = false
+		$TryAgain.visible = true
 
 func _on_Difficulty_dying(difficulty):
 	init = true
+	a = 1
+
+func _on_Retry_pressed():
+	get_tree().change_scene('res://Game/Main.tscn')
